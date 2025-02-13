3ba58c73005fb055d08f4ca740561d23481423f9
Heytgala <heytgala26@gmail.com>
2025-02-13 02:01:50 -0600

    Connected MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -10,6 +10,11 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+
+
+dotenv.config();
 const app = express();
 const httpServer = http.createServer(app);
 
@@ -31,5 +36,6 @@ app.use(
 );
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
 
 console.log(`Server ready at http://localhost:4000`);
